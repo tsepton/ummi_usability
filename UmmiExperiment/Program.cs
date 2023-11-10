55183a79f21fe0f3e595321a1e56b1ac0d36a29b
Thibaut Septon <tsepton@gmail.com>
2023-11-10 10:13:36 +0100

    Create base project

---
+++ UmmiExperiment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+public class MMIInterface
+{
+    static void Main()
+    {
+        Console.WriteLine("Working");
+    }
+    public List<Type> Interfaces = new();
+}
+
+
+[AttributeUsage(validOn: AttributeTargets.Method)]
+public class MultimodalInterface : Attribute
+{
+    public string[] Utterances { get; }
+
+    public MultimodalInterface(string utterance)
+    {
+        Utterances = new[] { utterance };
+    }
+
+    public MultimodalInterface(string[] utterances)
+    {
+        Utterances = utterances;
+    }
+}
+
+
+
+public class Experiment : MMIInterface
+{
+
+    public void Start()
+    {
+        Interfaces.Add(typeof(TestInterface));
+    }
+
+    public static class TestInterface
+    {
+        [MultimodalInterface("Hi")]
+        public static void Hi() { }
+
+    }
+
+}
+
