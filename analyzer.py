4e0a1da8b63faa0186c5732f8f0eef86e49157af
Thibaut Septon <tsepton@gmail.com>
2023-11-24 17:26:56 +0100

    Add number of answers in graph

--- analyzer.py
+++ analyzer.py
@@ -1,37 +1,48 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 
 def get_results():
     sus_score = 0
     sus_score_details = []
-    results = {i: [0, 0, 0, 0, 0] for i in range(1, 11)}
+    sus_results = {i: [0, 0, 0, 0, 0] for i in range(1, 11)}
+    sample_results = {i: [0, 0, 0, 0, 0] for i in range(0, 5)}
+    sample_questions_keys = ["mmi_familiarity","mmi_experience","oo_experience","typed_experience","csharp_experience"]
 
     with open('results.csv', 'r') as csv_file:
         data = csv.DictReader(csv_file, delimiter=',', quotechar='"')
         number_of_users = 0
+    
         for userIndex, row in enumerate(data):
             sus_score_per_user = 0
-            for susIndex in range(1, 11):
-                userAnswer = int(row[f'sus_{susIndex}'])
+            for sus_index in range(1, 11):
+                userAnswer = int(row[f'sus_{sus_index}'])
                 sus_score_per_user += userAnswer - \
-                    1 if (susIndex % 2 == 1) else 5 - userAnswer
-                results[susIndex][userAnswer - 1] += 1
+                    1 if (sus_index % 2 == 1) else 5 - userAnswer
+                sus_results[sus_index][userAnswer - 1] += 1
+            
+            for sample_index in range(0, len(sample_questions_keys)):
+                sample_results[sample_index][int(row[sample_questions_keys[sample_index]]) - 1] += 1
+            
             sus_score_per_user *= 2.5
             sus_score_details.append(sus_score_per_user)
             sus_score += sus_score_per_user
             number_of_users += 1
         sus_score /= number_of_users
 
-        for key in results:
-            for index, value in enumerate(results[key]):
-                results[key][index] = value / number_of_users * 100
+        for key in sus_results:
+            for index, value in enumerate(sus_results[key]):
+                sus_results[key][index] = value / number_of_users * 100
 
-    return results, sus_score, sus_score_details
+        for key in sample_results:
+            for index, value in enumerate(sample_results[key]):
+                sample_results[key][index] = value / number_of_users * 100
+
+    return sus_results, sample_results, sus_score, sus_score_details
 
 
-def get_chart(results, category_names):
+def get_stacked_bar_chart(results, category_names, nb_participants):
     labels = list(results.keys())
     data = np.array(list(results.values()))
     data_cum = data.cumsum(axis=1)
@@ -48,7 +59,15 @@ def get_chart(results, category_names):
         starts = data_cum[:, i] - widths - offsets
         rects = ax.barh(labels, widths, left=starts, height=0.5,
                         label=colname, color=color)
-        
+
+        for rect, value in zip(rects, widths):
+            width = rect.get_width()
+            nb = math.ceil(round(value/100.0*nb_participants, 2))
+            if (nb == 0): 
+                continue
+            ax.text(width/2 + rect.get_x(), rect.get_y() + rect.get_height()/2,
+                    f'{nb}', ha='center', va='center', color='black', fontsize=8)
+
     ax.axvline(0, linestyle='--', color='black', alpha=.25)
     ax.set_xlim(-105, 105)
     ax.set_xticks(np.arange(-100, 101, 10))
@@ -68,16 +87,16 @@ def get_chart(results, category_names):
 
 
 if (__name__ == "__main__"):
-    results, sus_score, sus_per_user = get_results()
-    category_names = [
+    sus_results, sample_results, sus_score, sus_per_user = get_results()
+   
+    sus_category_names = [
         'Strongly disagree',
         'Disagree',
         'Neither agree nor disagree',
         'Agree',
         'Strongly agree'
     ]
-
-    question_labels = [
+    sus_question_labels = [
         "Q1. Would use frequently",
         "Q2. Is complex",
         "Q3. Is easy to use",
@@ -89,12 +108,32 @@ if (__name__ == "__main__"):
         "Q9. Felt confident",
         "Q10. Need to learn a lot"
     ]
-    results = {question_labels[i]: results[i+1] for i in range(0, 10)}
+    sus_results = {sus_question_labels[i]: sus_results[i+1] for i in range(0, 10)}
 
+
+    sample_category_names = [
+        'No notion',
+        'Novice',
+        'Intermediate',
+        'Proficient',
+        'Expert'
+    ]
+    sample_question_labels = [
+        "MMI Familiarity",
+        "MMI Experience",
+        "OO Familiarity",
+        "Strongly Typed Familiarity",
+        "C# Familiarity",
+    ]
+    sample_results = {sample_question_labels[i]: sample_results[i] for i in range(0, 5)}
+    print( sample_results)
 
     print(f'Average SUS Score: {sus_score}')
     for userIndex, score in enumerate(sus_per_user):
         print(f'SUS Score for {userIndex}: {score}')
 
-    fig, ax = get_chart(results, category_names)
+    fig, ax = get_stacked_bar_chart(sus_results, sus_category_names, len(sus_per_user))
+    plt.show()
+
+    fig, ax = get_stacked_bar_chart(sample_results, sample_category_names, len(sus_per_user))
     plt.show()
